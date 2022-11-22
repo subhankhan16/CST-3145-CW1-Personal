0569190bc0f83cf91192e4a7cf74d04bed28fe52
subhankhan16 <115208797+subhankhan16@users.noreply.github.com>
2022-11-22 13:38:37 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# After_School_Activity
+Coursework 1 for CST3145-Web based mobile app development 
---
+++ lessons.js
@@ -0,0 +1,112 @@
+let lessons = [
+	{
+		id: 1,
+		subject: 'Math',
+		location: 'Dubai',
+		price: '350',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/math.jpg'
+	},
+	{
+		id: 2,
+		subject: 'English',
+		location: 'Abu Dhabi',
+		price: '310',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/english.jpg'
+	},
+	{
+		id: 3,
+		subject: 'Arabic',
+		location: 'Dubai',
+		price: '320',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/arabic.jpg'
+	},
+	{
+		id: 4,
+		subject: 'Music',
+		location: 'Sharjah',
+		price: '300',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/music.jpg'
+	},
+	{
+		id: 5,
+		subject: 'Art',
+		location: 'Dubai',
+		price: '305',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/art.jpg'
+	},
+	{
+		id: 6,
+		subject: 'Computers',
+		location: 'Dubai',
+		price: '280',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/computer.jpg'
+	},
+	{
+		id: 7,
+		subject: 'Science',
+		location: 'Abu Dhabi',
+		price: '370',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/science.jpg'
+	},
+	{
+		id: 8,
+		subject: 'Accountancy',
+		location: 'Dubai',
+		price: '320',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/accountancy.jpg'
+	},
+	{
+		id: 9,
+		subject: 'Biology',
+		location: 'Dubai',
+		price: '290',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/biology.jpg'
+	},
+	{
+		id: 10,
+		subject: 'Physics',
+		location: 'Dubai',
+		price: '295',
+		space: '5',
+		availableInventory:'5',
+		inCart: false,
+		inCartQuantity: 0,
+		img: 'img/physics.jpg'
+	}
+];
